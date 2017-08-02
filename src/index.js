b3342208f2b2f452e09ab1a5360e921f44ace676
pokohide <hyde31415@gmail.com>
2017-08-03 01:51:03 +0900

    サンプル

---
+++ src/index.js
@@ -0,0 +1,14 @@
+class Flowly {
+  constructor() {
+
+  }
+
+  push(msg) {
+
+  }
+
+  resize({ width, height}) {
+    this.width = width || 500 // ここはエリアの長さ
+    this.height = height || 600
+  }
+}
